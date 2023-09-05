d4b59656ebe5182bccb64da80d55192219290ee2
Robert <ishbuldinrobert2002@gmail.com>
2023-09-05 10:51:58 +0500

    коррекктный ввод и вывод массива

--- control_work1/Program.cs
+++ control_work1/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿void FillArray(string[] str)
+{
+    for (int i = 0; i < str.Length; i++)
+    {
+        Console.Write((i + 1) + " строка: ");
+        str[i] = Console.ReadLine();
+    }
+}
+
+Console.Write("Сколько нужно строк в массиве: ");
+int size = int.Parse(Console.ReadLine());
+string[] words = new string[size];
+
+Console.WriteLine("Введите массив: ");
+FillArray(words);
+Console.WriteLine(string.Join(", ", words));
