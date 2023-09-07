5c9295c112abfb63256c3528cec9fe465406b5f7
Robert <ishbuldinrobert2002@gmail.com>
2023-09-07 15:54:24 +0500

    программа полностью написана и готова к запуску

--- control_work1/Program.cs
+++ control_work1/Program.cs
@@ -7,10 +7,40 @@
     }
 }
 
-Console.Write("Сколько нужно строк в массиве: ");
+string[] CreateNewArray(string[] str)
+{
+    string[] noElementsInArray = {"нет подходящих строк в веденном массиве"};
+    int sizeOfTheNewArray = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length <= 3)
+            sizeOfTheNewArray++;
+    }
+    if (sizeOfTheNewArray != 0)
+    {
+        string[] newWords = new string[sizeOfTheNewArray];
+        int count = 0;
+        for (int j = 0; j < str.Length; j++)
+        {
+            if (str[j].Length <= 3)
+            {
+                newWords[count] = str[j];
+                count++;
+            }
+        }
+        return newWords;
+    }
+    else return noElementsInArray;
+}
+
+Console.Write("Количество строк в массиве: ");
 int size = int.Parse(Console.ReadLine());
 string[] words = new string[size];
 
 Console.WriteLine("Введите массив: ");
 FillArray(words);
-Console.WriteLine(string.Join(", ", words));
+Console.WriteLine("Введенный массив: " + string.Join(", ", words));
+
+CreateNewArray(words);
+
+Console.WriteLine("Полученный массив: " + string.Join(", ", CreateNewArray(words)));
